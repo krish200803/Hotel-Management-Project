2e498abe5e620496adbd2c36c65cb98547dc4aa3
krish200803 <umeshbhadane2020@gmail.com>
2025-09-11 11:33:35 +0530

    project

--- Hotel Management System/Hotel-Management-System/src/main/java/com/example/Hotel/Management/System/dao/daoImpl/HotelDAOImpl.java
+++ Hotel Management System/Hotel-Management-System/src/main/java/com/example/Hotel/Management/System/dao/daoImpl/HotelDAOImpl.java
@@ -5,4 +5,23 @@ import com.example.Hotel.Management.System.dao.HotelDAO;
 
 public class HotelDAOImpl implements HotelDAO {
 
+    @Override
+    public void save() {
+        
+    }
+
+    @Override
+    public Hotel findByID(int id) {
+        return null;
+    }
+
+    @Override
+    public void updateAddress(int id) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
 }
