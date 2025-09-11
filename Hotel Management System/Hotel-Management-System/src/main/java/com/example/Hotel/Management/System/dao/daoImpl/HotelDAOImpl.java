84e866b7ffad60c9f1936bf64fa53243c434b39f
krish200803 <umeshbhadane2020@gmail.com>
2025-09-11 15:10:34 +0530

    Task done
    Delete by phone no feature added

--- Hotel Management System/Hotel-Management-System/src/main/java/com/example/Hotel/Management/System/dao/daoImpl/HotelDAOImpl.java
+++ Hotel Management System/Hotel-Management-System/src/main/java/com/example/Hotel/Management/System/dao/daoImpl/HotelDAOImpl.java
@@ -2,7 +2,65 @@ package com.example.Hotel.Management.System.dao.daoImpl;
 
 import com.example.Hotel.Management.System.Entity.Hotel;
 import com.example.Hotel.Management.System.dao.HotelDAO;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.Query;
+import jakarta.persistence.Table;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class HotelDAOImpl implements HotelDAO {
 
+<<<<<<< Updated upstream
+=======
+    private final EntityManager theManager;
+
+    @Autowired
+    public HotelDAOImpl(EntityManager theManager) {
+        this.theManager = theManager;
+    }
+
+    @Override
+    public void save() {
+
+    }
+
+    @Override
+    public Hotel findByID(int id) {
+        return null;
+    }
+
+    @Override
+    public void updateAddress(int id) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    @Transactional
+    public void deleteByPhone(long phoneno) {
+        Query query = theManager.createQuery("select h from Hotel h where h.phoneNo=:phno");
+        query.setParameter("phno",phoneno);
+        try{
+            Hotel h1 = (Hotel)query.getSingleResult();
+
+                theManager.remove(h1);
+                System.out.println(h1);
+                System.out.println("Record Deleted Successfully.");
+
+        }
+        catch (NoResultException noe) {
+            System.out.println("No Record Found!");
+        }
+
+    }
+
+
+>>>>>>> Stashed changes
 }
